d062294e399d1059f37c33589e7df2e61b1813db
kim jae young <kjy0922@gmail.com>
2022-01-23 20:17:10 +0900

    애플코딩 4강-1/버튼 이벤트

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
   </div>
 
-  <div v-for="(a,i) in products" :key="i">
-    <h4>{{ a }}</h4>
+  <div>
+    <h4>{{products[0]}}</h4>
     <p>50만원</p>
+    <button @click="신고수++">허위매물신고</button> <span>신고수 : {{신고수}}</span>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>60만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>70만원</p>
   </div>
 </template>
 
@@ -15,6 +24,7 @@ export default {
   name: 'App',
   data(){
     return{
+      신고수: 0,
       메뉴들 : ['Home', 'Shop' ,'About'],
       products : ['역삼동원룸','천호동원룸','마포구원룸']
     }
