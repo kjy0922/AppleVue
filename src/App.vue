6b92a24aa14a891cb4685bb58c7a97fd5a908fa6
kim jae young <kjy0922@gmail.com>
2022-01-23 23:19:20 +0900

    애플강의 6강/데이터 import

--- src/App.vue
+++ src/App.vue
@@ -12,36 +12,24 @@
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
   </div>
 
-  <div>
-    <img src="./assets/room0.jpg" class="room-img">
-    <h4 @click="모달창열렸니 = true">{{products[0]}}</h4>
-    <p>50만원</p>
-    <button @click="신고수[0]++">허위매물신고</button>
-    <span>신고수 : {{ 신고수[0] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room1.jpg" class="room-img">
-    <h4>{{products[1]}}</h4>
-    <p>50만원</p>
-    <button @click="신고수[1]++">허위매물신고</button>
-    <span>신고수 : {{ 신고수[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" class="room-img">
-    <h4>{{products[2]}}</h4>
-    <p>50만원</p>
-    <button @click="신고수[2]++">허위매물신고</button>
-    <span>신고수 : {{ 신고수[2] }}</span>
-  </div>
-  
+  <div v-for="b,c in 원룸들" :key="c">
+    <img :src="원룸들[c].image" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{원룸들[c].title}}</h4>
+    <p>{{원룸들[c].price}}원</p>
+    <button @click="신고수[c]++">허위매물신고</button>
+    <span>신고수 : {{ 신고수[c] }}</span>
+  </div>  
 </template>
 
 <script>
+
+import data from './assets/oneroom.js';
 
 export default {
   name: 'App',
   data(){
     return{
+      원룸들 : data,
       모달창열렸니 : false,
       신고수: [0,0,0],
       메뉴들 : ['Home', 'Shop' ,'About'],
